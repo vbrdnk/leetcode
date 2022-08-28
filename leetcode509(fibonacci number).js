2c99831782ca192eb38a8990d8706d5d741d4415
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2022-08-28 12:41:28 -0400

    add memo version for fibonacci numbers

--- leetcode509(fibonacci number).js
+++ leetcode509(fibonacci number).js
@@ -13,3 +13,28 @@ const fib = function(n) {
 
     return fib(n - 1) + fib(n - 2);
 };
+
+
+const fibMemo = function(n) {
+    const memo = [];
+
+    function fib(num, memorized) {
+        if (num < 1) {
+            return 0;
+        }
+
+        if (num === 1) {
+            return 1;
+        }
+
+        if (memorized[num]) {
+            return memorized[num];
+        }
+
+        memorized[num] = fib(num - 1, memorized) + fib(num - 2, memorized);
+
+        return memorized[num];
+    }
+
+    return fib(n, memo);
+}
