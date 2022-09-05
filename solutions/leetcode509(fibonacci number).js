832c0c5bfe60647ff8404d9b64922cff4367ebdc
vbrdnk <burdenyuchok@gmail.com>
2022-09-05 12:40:57 -0400

    add new linked list solutions

--- solutions/leetcode509(fibonacci number).js
+++ solutions/leetcode509(fibonacci number).js
@@ -38,3 +38,20 @@ const fibMemo = function(n) {
 
     return fib(n, memo);
 }
+
+// bottom-up solution
+const fibMemoBottomUp = function(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    const memo = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        memo[i] = memo[i - 1] + memo[i - 2];
+    }
+
+    return memo[n - 1] + memo[n - 2];
+}
+
+console.log(fibMemoBottomUp(13));
+console.log(fib(13));
--- templates/binary trees/bfs/levelorderTraversal.js
+++ templates/binary trees/bfs/levelorderTraversal.js
@@ -1,21 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
 const binaryTreeLevelOrderTemplate = (root) => {
     if (!root) {
         return [];
     }
-    
+
     let queue = [root];
     let result = [];
-    
+
     while (queue.length) {
         let len = queue.length;
         result.push(queue.map(node => node.val));
-        
+
         while (len--) {
             const node = queue.shift();
             if (node.left) queue.push(node.left);
             if (node.right) queue.push(node.right);
         }
     }
-    
+
     return result;
 }
