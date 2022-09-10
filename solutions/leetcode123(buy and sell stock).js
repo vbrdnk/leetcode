eb4fed578d4f3898334a98f0b2c730855612bf62
vbrdnk <burdenyuchok@gmail.com>
2022-09-10 18:44:32 -0400

    add new solutions

--- solutions/leetcode123(buy and sell stock).js
+++ solutions/leetcode123(buy and sell stock).js
@@ -3,27 +3,27 @@
  * @return {number}
  */
 const maxProfit = function(prices) {
-    let maxProfit = -Infinity;
-    let currProfit = 0;
-    let transactions = 2;
+    if (prices.length === 0) return 0
 
-    for (let i = 0; i < prices.length; i++) {
-        currProfit += prices[i];
-
-        if (currProfit > maxProfit) {
-            maxProfit = currProfit;
-            transactions--;
-        }
-
-
-        if (transactions === 0) {
-            break;
-        }
-
-        if (currProfit < 0) {
-            currProfit = 0;
-        }
+    let dp = new Array(prices.length).fill(0);
+    let min = prices[0];
+    let max = 0;
+    for (let i = 1; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        max = Math.max(max, prices[i] - min);
+        dp[i] = max;
     }
 
-    return maxProfit;
+    // 1st run dp = [0,0,2,2,2,3,3,4];
+
+    min = prices[0];
+    max = 0;
+    for (let i = 1; i < prices.length; i++) {
+        min = Math.min(min, prices[i] - dp[i]);
+        max = Math.max(max, prices[i] - min);
+        dp[i] = max;
+    }
+
+
+    return dp.pop();
 };
